386a78da75152efe1318e42a81840551d938f4a2
Daniel Luiz <106317396+DanielL159@users.noreply.github.com>
2024-04-12 21:11:25 -0300

    Add files via upload

---
+++ tupla.py
@@ -0,0 +1,16 @@
+#Tuplas ()
+#nao aceita mudanca
+#*x indica que vai receber varias variaveis
+#funcao tuple
+
+#Conjunto {}
+#nao aceita duplicata
+#funcao set()
+# diferenca a-b
+#uniao a|b
+#intersecao a&b
+#diferenca a-b
+#diferença simetrica a^b
+
+#Lista []
+tupla = ()
